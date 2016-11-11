3a1c257f31d09de7d12c32d972cb7991c92363e2
jonallen0 <jayayedev@gmail.com>
2016-11-11 09:25:30 +0000

    Initial commit from the atomist bot

---
+++ src/main/java/com/jcpa/ConsumerRouterApplication.java
@@ -0,0 +1,12 @@
+package com.jcpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerRouterApplication.class, args);
+    }
+}
